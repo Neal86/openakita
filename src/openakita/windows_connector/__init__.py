a1a6c97e3bcf082a0dce9c7658829fa2238c4f30
Neal86 <52608308+Neal86@users.noreply.github.com>
2026-08-11 12:47:07 -0700

    feat: register Windows Connector Agent tools

--- src/openakita/windows_connector/__init__.py
+++ src/openakita/windows_connector/__init__.py
@@ -1,5 +1,12 @@
 """Generic Windows Connector support for remote desktop/browser control."""
 
 from .manager import windows_connector_manager
+from .tools import WINDOWS_CONNECTOR_TOOLS, register_windows_connector_tools
 
-__all__ = ["windows_connector_manager"]
+register_windows_connector_tools()
+
+__all__ = [
+    "WINDOWS_CONNECTOR_TOOLS",
+    "register_windows_connector_tools",
+    "windows_connector_manager",
+]
