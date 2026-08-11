3a812a27fa85f12bf4d3dcad4dba4aba9a5c503d
Neal86 <52608308+Neal86@users.noreply.github.com>
2026-08-10 20:17:38 -0700

    refactor(tasks): make v3 service the public Task Center

--- hermes-extensions/task_center/service.py
+++ hermes-extensions/task_center/service.py
@@ -8,13 +8,13 @@ if str(PLUGIN_ROOT) not in sys.path:
     sys.path.insert(0, str(PLUGIN_ROOT))
 
 from task_center.service_v2 import (  # noqa: E402,F401
-    TaskCenter,
     _as_dt,
     _is_recurring_schedule,
     _iso,
     _now,
     _schedule_text,
 )
+from task_center.service_v3 import TaskCenter  # noqa: E402,F401
 
 __all__ = [
     "TaskCenter",
