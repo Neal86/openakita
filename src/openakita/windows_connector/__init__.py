c813513e1d7ec304462d61f5f5783cf6f4beca90
Neal86 <52608308+Neal86@users.noreply.github.com>
2026-08-16 15:14:29 -0700

    feat(windows): install bound browser adapter

--- src/openakita/windows_connector/__init__.py
+++ src/openakita/windows_connector/__init__.py
@@ -1,12 +1,15 @@
 """Generic Windows Connector support for remote desktop/browser control."""
 
+from .browser_adapter import install_browser_adapter
 from .manager import windows_connector_manager
 from .tools import WINDOWS_CONNECTOR_TOOLS, register_windows_connector_tools
 
 register_windows_connector_tools()
+install_browser_adapter()
 
 __all__ = [
     "WINDOWS_CONNECTOR_TOOLS",
+    "install_browser_adapter",
     "register_windows_connector_tools",
     "windows_connector_manager",
 ]
